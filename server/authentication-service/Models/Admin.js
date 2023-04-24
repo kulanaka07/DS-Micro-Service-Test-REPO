4973e1cba475c76ada94d9e5d818b0dffffa62ad
mehara00 <119718801+MeharaSahabandu@users.noreply.github.com>
2023-04-24 09:22:08 +0530

    admin-authentication service

--- server/authentication-service/Models/Admin.js
+++ server/authentication-service/Models/Admin.js
@@ -1,1 +1,26 @@
-//model
+//admin model
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+});
+
+module.exports = Admin = mongoose.model("admin", AdminSchema);
