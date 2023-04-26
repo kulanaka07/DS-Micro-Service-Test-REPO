0f9f2f11849d7939a0908271a90dad98d354a62e
Kanishka Jayarathna <110347124+KanishkaGJ@users.noreply.github.com>
2023-04-27 00:51:07 +0530

    Update server.js

---
+++ server/inventory-service/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./Config/db");
+const app = express();
+require("dotenv").config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(bodyParser.json());
+
+//admin route
+const admin_router = require("./Routes/admin-route");
+app.use("/admin", admin_router);
+
+const PORT = process.env.PORT || 8060;
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, () => {
+  useCreateIndex: true;
+  useNewUrlParser: true;
+  useUnifiedTopology: true;
+  useFindAndModify: true;
+});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB connection establishment is successful!!!");
+});
+
+
+
+const productRouter = require("./Routes/product-route");
+app.use("/product", productRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port: ${PORT}`);
+});
